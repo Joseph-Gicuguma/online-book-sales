a6528a62e96bbcf1e411633777fd584c9c90be37
Joseph-Gicuguma <josephgicuguma@gmail.com>
2025-03-15 14:50:50 +0300

    make theme consistent accross all components

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles/theme.css'; // Import theme first
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
